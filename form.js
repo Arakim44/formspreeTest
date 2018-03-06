d79311fc446874960b88c23ae617ef5f66576b4b
Ara Kim <ahrakim44@gmail.com>
2018-03-06 12:34:44 -0600

    Update form.js

--- form.js
+++ form.js
@@ -1,16 +1,37 @@
-$('form#contact_form').validate({
-  messages: { },
-  submitHandler: function(form) {
-    $.ajax({
-      url: "//formspree.io/ahrakim44@gmail.com",
-      method: "POST",
-      data: $(form).serialize(),
-      dataType: "json",
-      success: function(data) {
-          $("form#contact_form :input").prop("disabled", true);
-          $('#thanks').show();
-      }
+$(document).ready(function() {
+
+    $('#someForm').on('submit', function(e) {
+        e.preventDefault();
+        
+        //get the name field value
+        var name = $('#name').val();
+        //get the name field value
+        var email = $('#email').val();
+        //get the comments
+        var comments = $('#comments').val();
+                    
+        //pretend we don't need validation
+        
+        //send to formspree
+        $.ajax({
+            url:'https://formspree.io/raymondcamden@gmail.com',
+            method:'POST',
+            data:{
+                name:name,
+                _replyto:email,
+                 email:email,
+                comments:comments,
+                _subject:'My Form Submission',
+            },
+            dataType:"json",
+            success:function() {
+                console.log('success'); 
+                $('#formBlock').hide();
+                $('#thankyouBlock').show();
+            }   
+
+        });     
+        
     });
-    return false;
-  }
-});
+
+}); 
